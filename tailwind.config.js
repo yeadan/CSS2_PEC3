34540cedb200b9041da33354d5ff3c9b16a30c95
Sergio Bosch <sergio.bosch@gmail.com>
2022-05-13 14:54:47 +0200

    index without accordion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
@@ -5,10 +7,27 @@ module.exports = {
       'georgia': ['Georgia', 'serif'],
     },
     extend: {
+      spacing: {
+        p15: '15%',
+        p2h: '2.5%',
+      },
       colors: {
         accent: "#cc5801",
+        block: "#f1efe6",
       }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.presentation': {
+          paddingLeft: theme('spacing.p15'),
+          paddingRight: theme('spacing.p15'),
+          paddingTop: theme('spacing.p2h'),
+          paddingBottom: theme('spacing.p2h'),
+          marginTop: theme('spacing.0'),
+          text: theme('text-justify'),
+        },
+      })
+    })]
 }
